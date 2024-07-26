609361c88b7950df20e77937e4e6b99fc9cbcacf
Ocean <oceanjayant629@gmail.com>
2024-07-27 03:55:23 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,112 @@
+const typeBackgrounds = {
+    bug: "backgrounds/bug-bg.webp",
+    dragon: "backgrounds/dragon-bg.webp",
+    electric: "backgrounds/electric-bg.webp",
+    fairy: "backgrounds/fairy-bg.webp",
+    fighting: "backgrounds/fighting-bg.webp",
+    fire: "backgrounds/fire-background.webp",
+    flying: "backgrounds/sky-bg.jpg",
+    grass: "backgrounds/grass-background.webp",
+    ground: "backgrounds/ground-bg.webp",
+    ghost: "backgrounds/ghost-bg.webp",
+    ice: "backgrounds/ice-bg.webp",
+    normal: "backgrounds/normal-bg.webp", // Assuming you want peach color as bg for normal type
+    poison: "backgrounds/poison-bg.webp",
+    psychic: "backgrounds/psychic-bg.webp",
+    rock: "backgrounds/rock-bg.webp",
+    water: "backgrounds/water-background.webp",
+};
+
+const typeColor = {
+    bug: "#26de81",
+    dragon: "#ffeaa7",
+    electric: "#fed330",
+    fairy: "#FF0069",
+    fighting: "#30336b",
+    fire: "#f0932b",
+    flying: "#81ecec",
+    grass: "#00b894",
+    ground: "#EFB549",
+    ghost: "#a55eea",
+    ice: "#74b9ff",
+    normal: "#95afc0",
+    poison: "#6c5ce7",
+    psychic: "#a29bfe",
+    rock: "#2d3436",
+    water: "#0190FF",
+};
+
+const url = "https://pokeapi.co/api/v2/pokemon/";
+const card = document.getElementById("card");
+const btn = document.getElementById("btn");
+
+let getPokeData = () => {
+    let id = Math.floor(Math.random() * 150) + 1;
+    const finalUrl = url + id;
+    fetch(finalUrl)
+        .then((response) => response.json())
+        .then((data) => {
+            generateCard(data);
+        });
+};
+
+let generateCard = (data) => {
+    const hp = data.stats[0].base_stat;
+    const imgSrc = data.sprites.other.dream_world.front_default;
+    const pokeName = data.name[0].toUpperCase() + data.name.slice(1);
+    const statAttack = data.stats[1].base_stat;
+    const statDefense = data.stats[2].base_stat;
+    const statSpeed = data.stats[5].base_stat;
+
+    const themeColor = typeColor[data.types[0].type.name];
+    const backgroundImage = typeBackgrounds[data.types[0].type.name];
+
+    card.innerHTML = `
+        <p class="hp">
+            <span>HP</span>
+            ${hp}
+        </p>
+        <img src=${imgSrc} />
+        <h2 class="poke-name">${pokeName}</h2>
+        <div class="types"></div>
+        <div class="stats">
+            <div>
+                <h3>${statAttack}</h3>
+                <p>Attack</p>
+            </div>
+            <div>
+                <h3>${statDefense}</h3>
+                <p>Defense</p>
+            </div>
+            <div>
+                <h3>${statSpeed}</h3>
+                <p>Speed</p>
+            </div>
+        </div>
+    `;
+    appendTypes(data.types);
+    styleCard(themeColor);
+    changeBackground(backgroundImage);
+};
+
+let appendTypes = (types) => {
+    types.forEach((item) => {
+        let span = document.createElement("SPAN");
+        span.textContent = item.type.name;
+        document.querySelector(".types").appendChild(span);
+    });
+};
+
+let styleCard = (color) => {
+    card.style.background = `radial-gradient(circle at 50% 0%, ${color} 36%, #ffffff 36%)`;
+    card.querySelectorAll(".types span").forEach((typeColor) => {
+        typeColor.style.backgroundColor = color;
+    });
+};
+
+let changeBackground = (image) => {
+    document.body.style.backgroundImage = `url(${image})`;
+};
+
+btn.addEventListener("click", getPokeData);
+window.addEventListener("load", getPokeData);
